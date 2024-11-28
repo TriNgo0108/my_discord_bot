b41f1388942d63d6ad77f47f4df2fc86c1e40e3a
TriNgo <tringo0108@gmail.com>
2024-11-28 14:37:06 +0700

    rename infrastructure

--- iac/main.tf
+++ iac/main.tf
@@ -22,8 +22,43 @@ resource "aws_iam_policy" "daily_trigger_lambda_policy" {
   })
 }
 
+resource "aws_iam_policy" "trigger_worker_lambda_policy" {
+  name = var.trigger_worker_lambda_policy
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "arn:aws:logs:*:*:*"
+        Effect   = "Allow"
+      },
+    ]
+  })
+}
+
   resource "aws_iam_role" "daily_trigger_lambda_role" {
   name = var.daily_trigger_lambda_iam_role
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action    = "sts:AssumeRole",
+        Effect    = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+  resource "aws_iam_role" "trigger_worker_lambda_role" {
+  name = var.trigger_worker_lambda_iam_role
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -43,6 +78,11 @@ resource "aws_iam_role_policy_attachment" "daily_trigger_attachment" {
   policy_arn = aws_iam_policy.daily_trigger_lambda_policy.arn
 }
 
+resource "aws_iam_role_policy_attachment" "trigger_worker_attachment" {
+  role       = aws_iam_role.trigger_worker_lambda_role.name
+  policy_arn = aws_iam_policy.trigger_worker_lambda_policy.arn
+}
+
 resource "aws_lambda_function" "daily_trigger_lambda" {
   function_name = var.daily_trigger_lambda
   role          = aws_iam_role.daily_trigger_lambda_role.arn
@@ -54,14 +94,28 @@ resource "aws_lambda_function" "daily_trigger_lambda" {
   source_code_hash = filesha256(var.daily_trigger_lambda_zip_file)                      
   environment {
     variables = {
-      "DISCORD_TOKEN" = var.DISCORD_TOKEN
-      "GUILD_ID" = var.GUILD_ID
       "ENVIRONMENT" = var.environment
-      "OPENAI_API_KEY" = var.OPENAI_API_KEY
+      "RENDER_BACKEND_ENDPOINT": var.worker_endpoint
     }
   }
 }
 
+resource "aws_lambda_function" "trigger_worker_lambda" {
+  function_name = var.trigger_worker_lambda
+  role          = aws_iam_role.trigger_worker_lambda_role.arn
+  handler       = "index.handler" 
+  runtime       = "nodejs20.x"
+  timeout       = 300                              
+  memory_size   = 128        
+  filename      = var.trigger_worker_lambda_zip_file
+  source_code_hash = filesha256(var.trigger_worker_lambda_zip_file)                      
+  environment {
+    variables = {
+      "ENVIRONMENT" = var.environment
+      "RENDER_BACKEND_ENDPOINT": var.worker_endpoint
+    }
+  }
+}
 
 resource "aws_iam_role" "daily_trigger_scheduler_role" {
   name = var.daily_trigger_scheduler_iam_role
@@ -79,6 +133,21 @@ resource "aws_iam_role" "daily_trigger_scheduler_role" {
   })
 }
 
+resource "aws_iam_role" "trigger_worker_scheduler_role" {
+  name = var.trigger_worker_scheduler_iam_role
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action    = "sts:AssumeRole",
+        Effect    = "Allow",
+        Principal = {
+          Service = "scheduler.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
 
 resource "aws_iam_policy" "daily_trigger_scheduler_policy" {
   name = var.daily_trigger_scheduler_policy
@@ -96,11 +165,31 @@ resource "aws_iam_policy" "daily_trigger_scheduler_policy" {
   })
 }
 
+resource "aws_iam_policy" "trigger_worker_scheduler_policy" {
+  name = var.trigger_worker_scheduler_policy
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = [
+            "lambda:InvokeFunction"
+        ],
+        Resource = aws_lambda_function.trigger_worker.arn
+        Effect   = "Allow"
+      },
+    ]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "daily_trigger_scheduler_attachment" {
   role       = aws_iam_role.daily_trigger_scheduler_role.name
   policy_arn = aws_iam_policy.daily_trigger_scheduler_policy.arn
 }
 
+resource "aws_iam_role_policy_attachment" "trigger_worker_scheduler_attachment" {
+  role       = aws_iam_role.trigger_worker_scheduler_role.name
+  policy_arn = aws_iam_policy.trigger_worker_scheduler_policy.arn
+}
 
 resource "aws_scheduler_schedule" "daily_trigger_scheduler" {
   name = var.daily_trigger_scheduler_schedule
@@ -116,6 +205,18 @@ resource "aws_scheduler_schedule" "daily_trigger_scheduler" {
 }
 
 
+resource "aws_scheduler_schedule" "trigger_worker_scheduler" {
+  name = var.trigger_worker_scheduler_schedule
+  flexible_time_window {
+    mode = "OFF"
+  }
+  target {
+   arn = aws_lambda_function.trigger_worker_lambda.arn
+   role_arn = aws_iam_role.trigger_worker_scheduler_role.arn
+  }
+  schedule_expression = "cron(13 * ? * * *)"
+  schedule_expression_timezone = "Asia/Ho_Chi_Minh"
+}
 terraform {
 backend "s3" {
   bucket = "my-discord-bot-core-terraform-state"
--- iac/variables.tf
+++ iac/variables.tf
@@ -4,10 +4,23 @@ variable "daily_trigger_lambda_policy" {
   default = "my-discord-bot-core-daily-trigger-lambda-policy"
 }
 
+variable "trigger_worker_lambda_policy" {
+  description = "Trigger worker lambda policy"
+  type = string
+  default = "my-trigger-worker-core-daily-trigger-lambda-policy"
+}
+
 variable "daily_trigger_scheduler_policy" {
   description = "Daily trigger scheduler policy"
   type = string
   default = "my-discord-bot-core-daily-trigger-scheduler-policy"
+}
+
+
+variable "trigger_worker_scheduler_policy" {
+  description = "Trigger Worker scheduler policy"
+  type = string
+  default = "my-trigger_worker-core-daily-trigger-scheduler-policy"
 }
 
 
@@ -16,11 +29,22 @@ variable "daily_trigger_lambda_iam_role" {
   type = string
   default = "daily-trigger-lambda-iam-role"
 }
+variable "trigger_worker_lambda_iam_role" {
+  description = "IAM Role trigger worker lambda"
+  type = string
+  default = "trigger-worker-lambda-iam-role"
+}
 
 variable "daily_trigger_scheduler_iam_role" {
   description = "IAM Role daily trigger scheduler"
   type = string
   default = "daily-trigger-scheduler-iam-role"
+}
+
+variable "trigger_worker_scheduler_iam_role" {
+  description = "IAM Role trigger worker scheduler"
+  type = string
+  default = "trigger-worker-scheduler-iam-role"
 }
 
 variable "daily_trigger_lambda" {
@@ -29,17 +53,36 @@ variable "daily_trigger_lambda" {
  default = "daily-trigger-lambda"
 }
 
+variable "trigger_worker_lambda" {
+ description = "Main lambda function name"
+ type = string
+ default = "trigger-worker-lambda"
+}
+
 variable "daily_trigger_scheduler_schedule" {
  description = "Daily trigger scheduler-schedule" 
  type = string
  default = "daily-trigger-scheduler-schedule"
 }
+variable "trigger_worker_scheduler_schedule" {
+ description = "trigger worker scheduler-schedule" 
+ type = string
+ default = "trigger-worker-scheduler-schedule"
+}
 
 variable "daily_trigger_lambda_zip_file" {
   description = "The zipped file of source code"
   type = string
-  default = "../discord_chat_bot.zip"
+  default = "../trigger-worker.zip"
 }
+
+variable "trigger_worker_lambda_zip_file" {
+  description = "The zipped file of source code"
+  type = string
+  default = "../trigger-worker.zip"
+}
+
+
 
 variable "aws_region" {
   description = "Default aws region"
@@ -48,26 +91,14 @@ variable "aws_region" {
 }
 
 
-variable "DISCORD_TOKEN" {
-  description = "Discord token"
-  type = string
-  default = ""
-}
-
-variable "GUILD_ID" {
-  description = "Guild Id"
-  type = string
-  default = ""
-}
-
 variable "environment" {
   description = "Level of testing"
   type = string
   default = "Prod"
 }
 
-variable "OPENAI_API_KEY" {
-  description = "Open API Key"
+variable "worker_endpoint" {
+  description = "The endpoint of worker"
   type = string
-  default = ""
+  default = "https://my-discord-bot-latest.onrender.com"
 }
