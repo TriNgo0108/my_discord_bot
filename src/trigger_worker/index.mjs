4fb18f25662bedf5f5178134337b25d35931f195
TriNgo <tringo0108@gmail.com>
2024-11-28 17:04:04 +0700

    update pipeline

--- src/trigger_worker/index.mjs
+++ src/trigger_worker/index.mjs
@@ -24,3 +24,21 @@ export const handler = async (event, context) => {
 
   return context.logStreamName;
 };
+
+
+let retryCounter = 0;
+  const maxRetry = 4;
+  const sleepTime = 1500;
+  dotenv.config();
+
+  let response = await fetch(process.env.RENDER_BACKEND_ENDPOINT);
+  while (!response.ok && retryCounter <= maxRetry) {
+    await new Promise((resolve) => setTimeout(resolve, sleepTime));
+    retryCounter++;
+    response = await fetch(process.env.RENDER_BACKEND_ENDPOINT);
+  }
+
+  if (response.ok) console.log("Trigger worker successfully");
+  else {
+    console.error("Failed to trigger worker");
+  }
