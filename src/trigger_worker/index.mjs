7019d66b29e7eec4e19ab17c46ebb96fd48df527
TriNgo <tringo0108@gmail.com>
2024-11-28 14:00:11 +0700

    re-structure

---
+++ src/trigger_worker/index.mjs
@@ -0,0 +1,22 @@
+// import { Client, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
+// import OpenAI from 'openai';
+
+export const handler = async (event, context) =>{
+  let retryCounter = 0;
+  const maxRetry = 4;
+  const sleepTime = 1500;
+  if(!process.env.environment) {
+    dotenv.config();  
+  }
+
+
+  let response = await fetch(process.env.RENDER_BACKEND_ENDPOINT);
+  while(!response.ok && retryCounter <= maxRetry) {
+    await new Promise(resolve => setTimeout(resolve, sleepTime));
+    retryCounter++;
+    response = await fetch(process.env.RENDER_BACKEND_ENDPOINT)
+  }
+
+  return context.logStreamName;
+};
