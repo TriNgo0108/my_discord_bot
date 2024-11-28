41bed001f082c27250b5848af807f996a566d54d
TriNgo <tringo0108@gmail.com>
2024-11-28 16:34:16 +0700

    update trigger pipeline

--- src/trigger_worker/index.mjs
+++ src/trigger_worker/index.mjs
@@ -2,20 +2,24 @@
 import dotenv from "dotenv";
 // import OpenAI from 'openai';
 
-export const handler = async (event, context) =>{
+export const handler = async (event, context) => {
   let retryCounter = 0;
   const maxRetry = 4;
   const sleepTime = 1500;
-  if(!process.env.environment) {
-    dotenv.config();  
+  if (!process.env.environment) {
+    dotenv.config();
   }
 
-
   let response = await fetch(process.env.RENDER_BACKEND_ENDPOINT);
-  while(!response.ok && retryCounter <= maxRetry) {
-    await new Promise(resolve => setTimeout(resolve, sleepTime));
+  while (!response.ok && retryCounter <= maxRetry) {
+    await new Promise((resolve) => setTimeout(resolve, sleepTime));
     retryCounter++;
-    response = await fetch(process.env.RENDER_BACKEND_ENDPOINT)
+    response = await fetch(process.env.RENDER_BACKEND_ENDPOINT);
+  }
+
+  if (response.ok) console.log("Trigger worker successfully");
+  else {
+    console.error("Failed to trigger worker");
   }
 
   return context.logStreamName;
