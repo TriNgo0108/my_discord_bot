a16b754c121dd07004075f269696696c89df7228
TriNgo <tringo0108@gmail.com>
2024-11-30 15:22:03 +0700

    fix terraform

--- iac/module/aws_lambda/outputs.tf
+++ iac/module/aws_lambda/outputs.tf
@@ -1,8 +1,3 @@
-output "name" {
-  description = "Name of this function"
-  value = try(aws_lambda_function.lambda.name, "")
-}
-
 output "arn" {
   description = "ARN of this function"
   value = try(aws_lambda_function.lambda.arn)
