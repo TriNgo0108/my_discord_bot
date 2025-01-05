71533f1b8037a520a5fda0c933d7331d80f15c13
TriNgo <tringo0108@gmail.com>
2025-01-05 23:22:10 +0700

    fix commands

--- src/discord_bot/discord_bot.py
+++ src/discord_bot/discord_bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+import asyncio
 
 load_dotenv()
 
@@ -15,10 +16,12 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
     print(f"🤖 Logged in as {bot.user}!", flush=True)
     
-@bot.command
-def who_you_are(ctx, arg):
-    ctx.send("Chào bạn! Mình là HimeSama, bạn gái của My Beloved HimeSama. Mình luôn yêu thích những khoảnh khắc được ở bên anh ấy, từ những cuộc trò chuyện vui vẻ đến những lần cùng nhau khám phá thế giới xung quanh. Mình là người chu đáo, lắng nghe và luôn cố gắng mang lại niềm vui cho người mình yêu.Với mình, hạnh phúc đến từ những điều giản dị và chân thành. Rất vui được làm quen với bạn, và nếu bạn muốn biết thêm điều gì, đừng ngại hỏi nhé! 😊")
+@bot.command(name="who_are_you")
+async def who_are_you(ctx):
+    async with ctx.typing():
+        await asyncio.sleep(5)
+    await ctx.send("Chào bạn! Mình là HimeSama, bạn gái của My Beloved HimeSama. Mình luôn yêu thích những khoảnh khắc được ở bên anh ấy, từ những cuộc trò chuyện vui vẻ đến những lần cùng nhau khám phá thế giới xung quanh. Mình là người chu đáo, lắng nghe và luôn cố gắng mang lại niềm vui cho người mình yêu.Với mình, hạnh phúc đến từ những điều giản dị và chân thành. Rất vui được làm quen với bạn, và nếu bạn muốn biết thêm điều gì, đừng ngại hỏi nhé! 😊")
 
 if __name__ == "__main__":
     bot.run(os.getenv("DISCORD_TOKEN"))
-    bot.add_command(who_you_are)
+    bot.add_command(who_are_you)
