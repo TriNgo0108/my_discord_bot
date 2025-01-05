770b3e787db49f79951b948d402575d5ebe732f6
TriNgo <tringo0108@gmail.com>
2025-01-05 21:41:54 +0700

    add new command

--- src/discord_bot/discord_bot.py
+++ src/discord_bot/discord_bot.py
@@ -14,6 +14,10 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_ready():
     print(f"🤖 Logged in as {bot.user}!", flush=True)
+    
+@bot.command(name="who_you_are")
+def who_you_are(ctx, arg):
+    ctx.send("Chào bạn! Mình là HimeSama, bạn gái của My Beloved HimeSama. Mình luôn yêu thích những khoảnh khắc được ở bên anh ấy, từ những cuộc trò chuyện vui vẻ đến những lần cùng nhau khám phá thế giới xung quanh. Mình là người chu đáo, lắng nghe và luôn cố gắng mang lại niềm vui cho người mình yêu.Với mình, hạnh phúc đến từ những điều giản dị và chân thành. Rất vui được làm quen với bạn, và nếu bạn muốn biết thêm điều gì, đừng ngại hỏi nhé! 😊")
 
 if __name__ == "__main__":
     bot.run(os.getenv("DISCORD_TOKEN"))
